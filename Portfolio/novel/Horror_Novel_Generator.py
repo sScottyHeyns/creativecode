5d43d23c1bf5f9e80701fea1f57a309f0f2bfc78
sScottyHeyns <54907745+sScottyHeyns@users.noreply.github.com>
2019-12-13 09:36:54 -0500

    Add files via upload

--- Portfolio/novel/Horror_Novel_Generator.py
+++ Portfolio/novel/Horror_Novel_Generator.py
@@ -28,25 +28,32 @@ def makePDF(filename):
     fileTitle= ("Abominations\\%s.pdf" % title[0:-1])
     pdf.output(fileTitle)
 
-def generate(model, file, sent_num):
+def generate(model, filename, sent_num):
     counter= 0
     counter2= 0
     par_length= 0
     chap_len= rng.randrange(15,31)
-    chapter_num= 1
+    chapter_num= 2
+    file= open(filename, "a")
+    file.write(("\n\nChapter %d\n\n" % (chapter_num - 1)))
     file.write("     ")
+    file.close()
     for i in range(sent_num):
         if(counter==0):
             par_length= rng.randrange(4,8)
-            file.write(("\n\nChapter %s\n\n" % chapter_num))
-            chapter_num+= 1
+        file= open(filename, "a")
         file.write(str(model.make_sentence()+" "))
+        file.close()
         if(counter==par_length):
+            file= open(filename, "a")
             file.write("\n     ")
+            file.close()
             counter2+= 1
             counter= -1
             if(counter2 == chap_len):
+                file= open(filename, "a")
                 file.write(("\n\nChapter %s\n\n" % chapter_num))
+                file.close()
                 chapter_num+= 1
                 chap_len= rng.randrange(15,31)
                 counter2= 0
@@ -73,24 +80,25 @@ def main():
     output1.close()
     output1= open("Abominations\\DifferentDracula.txt","a")
     output1.write("Different Dracula\n\n")
-    generate(dracula, output1, 200)
     output1.close()
+    generate(dracula, "Abominations\\DifferentDracula.txt", 200)
     makePDF("Abominations\\DifferentDracula.txt")
     
     output2= open("Abominations\\NewFrankenstein.txt","w+")
     output2.close()
     output2= open("Abominations\\NewFrankenstein.txt","a")
     output2.write("New Frankenstein\n\n")
-    generate(frankenstein, output2, 200)
     output2.close()
+    generate(frankenstein, "Abominations\\NewFrankenstein.txt", 200)
     makePDF("Abominations\\NewFrankenstein.txt")
     
     output3= open("Abominations\\OriginalCthulhu.txt","w+")
     output3.close()
     output3= open("Abominations\\OriginalCthulhu.txt","a")
     output3.write("Original Cthulhu\n\n")
-    generate(mountains, output3, 200)
     output3.close()
+    generate(mountains, "Abominations\\OriginalCthulhu.txt", 200)
+    makePDF("Abominations\\OriginalCthulhu.txt")
 
     #Combine Models and output combined story
     print("Writing Pure Horror...")
@@ -100,8 +108,8 @@ def main():
     output4.close()
     output4= open("Abominations\\Pure_Horror.txt","a")
     output4.write("Pure Horror\n\n")
-    generate(model, output4, 55000)
     output4.close()
+    generate(model, "Abominations\\Pure_Horror.txt", 55000)
     makePDF("Abominations\\Pure_Horror.txt")
 
     #Objectivity Model of Final Book
